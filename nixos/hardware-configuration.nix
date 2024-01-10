04416b19030ff207b81914c822419b8f8dc877b7
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-10 12:09:31 +0100

    delete uselless line, keeping minimal

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -7,10 +7,8 @@
 	boot = {
 		initrd = {
 			availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-			kernelModules = [];
 		};
 		kernelModules = [ "kvm-intel" ];
-		extraModulePackage = [ ];
 	};
 
 	networking.useDHCP = lib.mkDefault true;

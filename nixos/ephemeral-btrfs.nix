331dfd33fd5e7295bad13523a0f190c67b3ac408
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-18 17:55:28 +0100

    cleaning code

--- nixos/ephemeral-btrfs.nix
+++ nixos/ephemeral-btrfs.nix
@@ -1,23 +1,4 @@
 { lib, config, ... }:
-let
-	eraseYourDarlings = /* bash */ ''
-		mkdir -p /mnt	
-    mount -o subvol=/ /dev/disk/by-partlabel/root /mnt
-
-    btrfs subvolume list -o /mnt/root | cut -f9 -d' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
-    echo "deleting /root subvolume..." &&
-    btrfs subvolume delete /mnt/root
-
-    echo "restoring blank /root subvolume..."
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-    umount /mnt
-	'';
-in
 {
 	boot.initrd = {
 		enable = true;
@@ -36,7 +17,24 @@ in
 			before = [ "sysroot.mount" ];
 			unitConfig.DefaultDependencies = "no";
 			serviceConfig.Type = "oneshot";
-			script = eraseYourDarlings;
+			script =  /* bash */ ''
+				mkdir -p /mnt	
+    		mount -o subvol=/ /dev/disk/by-partlabel/root /mnt
+
+    		btrfs subvolume list -o /mnt/root | cut -f9 -d' ' |
+    		while read subvolume; do
+    		  echo "deleting /$subvolume subvolume..."
+    		  btrfs subvolume delete "/mnt/$subvolume"
+    		done &&
+    		echo "deleting /root subvolume..." &&
+    		btrfs subvolume delete /mnt/root
+
+    		echo "restoring blank /root subvolume..."
+    		btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+    		umount /mnt
+
+			'';
 		};
 	};
 }

e37ac2f07e0f8c78d591266394b5afe9f1819611
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-18 18:47:20 +0100

    use other options

--- nixos/ephemeral-btrfs.nix
+++ nixos/ephemeral-btrfs.nix
@@ -8,31 +8,34 @@
 			description = "Rollback btrfs rootfs";
 			wantedBy = [ "intitrd.target" ];
 			requires = [ 
-				"dev-disk-by\\x2dlabel-root.device"  
+				"initrd-root-device.target"
 			];
 			after = [
-				"dev-disk-by\\x2dlabel-root.device"  
+				"initrd-root-device.target"
 				#"systemd-cryptsetup@${config.networking.hostName}.service"
 			];
 			before = [ "sysroot.mount" ];
 			unitConfig.DefaultDependencies = "no";
 			serviceConfig.Type = "oneshot";
 			script =  /* bash */ ''
-				mkdir -p /mnt	
-    		mount -o subvol=/ /dev/disk/by-partlabel/root /mnt
+				mkdir /tmp -p
+				MNTPOINT=$(mktemp -d)
+    		(
+    		  mount -t btrfs -o subvol=/ /dev/disk/by-partlabel/root "$MNTPOINT"
+    		  trap 'umount "$MNTPOINT"' EXIT
 
-    		btrfs subvolume list -o /mnt/root | cut -f9 -d' ' |
-    		while read subvolume; do
-    		  echo "deleting /$subvolume subvolume..."
-    		  btrfs subvolume delete "/mnt/$subvolume"
-    		done &&
-    		echo "deleting /root subvolume..." &&
-    		btrfs subvolume delete /mnt/root
+    		  echo "Creating needed directories"
+    		  mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
 
-    		echo "restoring blank /root subvolume..."
-    		btrfs subvolume snapshot /mnt/root-blank /mnt/root
+    		  echo "Cleaning root subvolume"
+    		  btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
+    		  while read -r subvolume; do
+    		    btrfs subvolume delete "$MNTPOINT/$subvolume"
+    		  done && btrfs subvolume delete "$MNTPOINT/root"
 
-    		umount /mnt
+    		  echo "Restoring blank subvolume"
+    		  btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
+    		)
 			'';
 		};
 	};

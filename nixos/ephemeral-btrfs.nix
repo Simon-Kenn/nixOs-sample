e8bb78b9a438240274d578eb2ff3a923826e25ce
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-19 17:07:59 +0100

    rewriting ephermeral btrfs

--- nixos/ephemeral-btrfs.nix
+++ nixos/ephemeral-btrfs.nix
@@ -1,42 +1,45 @@
 { lib, config, ... }:
+let
+  wipeScript = /* bash */ ''
+    mkdir /tmp -p
+    MNTPOINT=$(mktemp -d)
+    (
+      mount -t btrfs -o subvol=/ /dev/disk/by-label/root "$MNTPOINT"
+      trap 'umount "$MNTPOINT"' EXIT
+
+      echo "Creating needed directories"
+      mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
+
+      echo "Cleaning root subvolume"
+      btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
+      while read -r subvolume; do
+        btrfs subvolume delete "$MNTPOINT/$subvolume"
+      done && btrfs subvolume delete "$MNTPOINT/root"
+
+      echo "Restoring blank subvolume"
+      btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
+    )
+  '';
+  phase1Systemd = config.boot.initrd.systemd.enable;
+in
 {
-	boot.initrd = {
-		enable = true;
-		supportedFilesystems = ["btrfs"];
-
-		systemd.services.restore-root = {
-			description = "Rollback btrfs rootfs";
-			wantedBy = [ "intitrd.target" ];
-			requires = [ 
-				"dev-disk-by\\x2dlabel-root.device"
-			];
-			after = [
-				"dev-disk-by\\x2dlabel-root.device"
-				#"systemd-cryptsetup@${config.networking.hostName}.service"
-			];
-			before = [ "sysroot.mount" ];
-			unitConfig.DefaultDependencies = "no";
-			serviceConfig.Type = "oneshot";
-			script =  /* bash */ ''
-				mkdir /tmp -p
-				MNTPOINT=$(mktemp -d)
-    		(
-    		  mount -t btrfs -o subvol=/ /dev/disk/by-label/root "$MNTPOINT"
-    		  trap 'umount "$MNTPOINT"' EXIT
-
-    		  echo "Creating needed directories"
-    		  mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
-
-    		  echo "Cleaning root subvolume"
-    		  btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
-    		  while read -r subvolume; do
-    		    btrfs subvolume delete "$MNTPOINT/$subvolume"
-    		  done && btrfs subvolume delete "$MNTPOINT/root"
-
-    		  echo "Restoring blank subvolume"
-    		  btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
-    		)
-			'';
-		};
-	};
+  boot.initrd = {
+    supportedFilesystems = [ "btrfs" ];
+    postDeviceCommands = lib.mkIf (!phase1Systemd) (lib.mkBefore wipeScript);
+    systemd.services.restore-root = lib.mkIf phase1Systemd {
+      description = "Rollback btrfs rootfs";
+      wantedBy = [ "initrd.target" ];
+      requires = [
+        "dev-disk-by\\x2dlabel-root.device"
+      ];
+      after = [
+        "dev-disk-by\\x2dlabel-root.device"
+#        "systemd-cryptsetup@${hostname}.service"
+      ];
+      before = [ "sysroot.mount" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = wipeScript;
+    };
+  };
 }

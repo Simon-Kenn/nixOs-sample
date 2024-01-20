73b5fb13d34d9647c5df09212d9ca3dbe5b5f7ab
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-20 10:21:00 +0100

    try to make it work

--- nixos/ephemeral-btrfs.nix
+++ nixos/ephemeral-btrfs.nix
@@ -1,25 +1,42 @@
 { lib, config, ... }:
 let
   wipeScript = /* bash */ ''
-    mkdir /tmp -p
-    MNTPOINT=$(mktemp -d)
-    (
-      mount -t btrfs -o subvol=/ /dev/disk/by-label/root "$MNTPOINT"
-      trap 'umount "$MNTPOINT"' EXIT
+    #mkdir /tmp -p
+    #MNTPOINT=$(mktemp -d)
+    #(
+    #  mount -t btrfs -o subvol=/ /dev/disk/by-label/root "$MNTPOINT"
+    #  trap 'umount "$MNTPOINT"' EXIT
 
-      echo "Creating needed directories"
-      mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
+    #  echo "Creating needed directories"
+    #  mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
 
-      echo "Cleaning root subvolume"
-      btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
-      while read -r subvolume; do
-				echo "  Deleting /$subvolume subvolume..."
-        btrfs subvolume delete "$MNTPOINT/$subvolume"
-      done && echo "  Deleting root subvolume" && btrfs subvolume delete "$MNTPOINT/root"
+    #  echo "Cleaning root subvolume"
+    #  btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
+    #  while read -r subvolume; do
+		#		echo "  Deleting /$subvolume subvolume..."
+    #    btrfs subvolume delete "$MNTPOINT/$subvolume"
+    #  done && echo "  Deleting root subvolume" && btrfs subvolume delete "$MNTPOINT/root"
 
-      echo "Restoring blank subvolume"
-      btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
-    )
+    #  echo "Restoring blank subvolume"
+    #  btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
+    #)
+		mkdir -p /mnt
+
+    mount -o subvol=/ /dev/disk/by-label/NIXROOT /mnt
+
+    btrfs subvolume list -o /mnt/root |
+    cut -f9 -d' ' |
+    while read subvolume; do
+      echo "deleting /$subvolume subvolume..."
+      btrfs subvolume delete "/mnt/$subvolume"
+    done &&
+    echo "deleting /root subvolume..." &&
+    btrfs subvolume delete /mnt/root
+
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+    umount /mnt
   '';
   phase1Systemd = config.boot.initrd.systemd.enable;
 in
@@ -31,10 +48,10 @@ in
       description = "Rollback btrfs rootfs";
       wantedBy = [ "initrd.target" ];
       requires = [
-        "dev-disk-by\\x2dlabel-root.device"
+        "dev-disk-by\\x2dlabel-NIXROOT.device"
       ];
       after = [
-        "dev-disk-by\\x2dlabel-root.device"
+        "dev-disk-by\\x2dlabel-NIXROOT.device"
 #        "systemd-cryptsetup@${hostname}.service"
       ];
       before = [ "sysroot.mount" ];

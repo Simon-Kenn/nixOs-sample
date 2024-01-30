b2c833be4586304bd4b5cab1018acddd2a1d077e
Zarthoustra42 <simon.kenn.public@pm.me>
2024-01-30 17:10:57 +0100

    deleting lvm simple btrfs return

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -6,8 +6,7 @@
         device = "/dev/disk/by-id/ata-Micron_1100_MTFDDAV256TBN_174619CDC946";
         content = {
           type = "gpt";
-          partitions = 
-					{
+          partitions = {
             esp = {
               priority = 1;
               start = "1M";
@@ -20,75 +19,50 @@
                 mountpoint = "/boot";
               };
             };
-						root = {
-							size = "100%";
-							content = {
+            root = {
+              size = "100%";
+              content = {
 								type = "luks";
 								name = "crypted";
 								settings = {
 									allowDiscards = true;
 								};
 								content = {
-									type = "lvm_pv";
-									vg = "pool";
-								};
-							};
-						};
-					};
+									type = "btrfs";
+                	extraArgs = [ "-f" "-L NIXROOT"]; 
+									postCreateHook = /* sh */ ''
+										MNTPOINT=$(mktemp -d)
+										mount "/dev/mapper/crypted" "$MNTPOINT" -o subvol=/
+										trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
+										btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
+
+									'';
+                	subvolumes = {
+										"/root" = {
+                  	  mountpoint = "/";
+											mountOptions = ["compress=zstd" "noatime"];
+                  	};
+                  	"/nix" = {
+                  	  mountpoint = "/nix";
+                  	  mountOptions = [ "compress=zstd" "noatime"];
+                  	};
+                  	"/persist" = {
+                  	  mountpoint = "/persist";
+                  	  mountOptions = [ "compress=zstd" "noatime" ];
+                  	};
+                  	"/swap" = {
+                  	  mountpoint = "/.swapvol";
+                  	  swap.swapfile.size = "20M";
+											mountOptions = [ "noatime" ];
+                  	};
+									};
+                };
+              };
+            };
+          };
+        };
       };
     };
-	};
-		lvm_vg = {
-			pool = {
-				type = "lvm_vg";
-				lvs = {
-					root = {
-						size = "100%FREE";
-						content = {
-							type = "btrfs";
-            	extraArgs = [ "-f" "-L NIXROOT"]; 
-							postCreateHook = /* sh */ ''
-								MNTPOINT=$(mktemp -d)
-								mount "/dev/mapper/pool-root" "$MNTPOINT" -o subvol=/
-								trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
-								btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
-
-							'';
-            	subvolumes = {
-								"/root" = {
-              	  mountpoint = "/";
-									mountOptions = ["compress=zstd" "noatime"];
-              	};
-              	"/nix" = {
-              	  mountpoint = "/nix";
-              	  mountOptions = [ "compress=zstd" "noatime"];
-              	};
-              	"/persist" = {
-              	  mountpoint = "/persist";
-              	  mountOptions = [ "compress=zstd" "noatime" ];
-              	};
-              	"/swap" = {
-              	  mountpoint = "/.swapvol";
-              	  swap.swapfile.size = "20M";
-									mountOptions = [ "noatime" ];
-              	};
-							};
-            };
-					};
-				};
-			};
-		};
-	};
+  };
 	fileSystems."/persist".neededForBoot = true;
 }
-#root = {
-            #  size = "100%";
-            #  content = {
-						#		type = "luks";
-						#		name = "crypted";
-						#		settings = {
-						#			allowDiscards = true;
-						#		};
-            #  };
-            #};
-

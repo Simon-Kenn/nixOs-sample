d64dfe62d0e9268b88291d1ecbe9256c7ce9f6be
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-02 10:12:45 +0100

    Cleaning

--- home/default.nix
+++ home/default.nix
@@ -6,12 +6,11 @@
 	home = {
 		username = "user";
 		homeDirectory = "/home/user";
+		stateVersion = "24.05";
 	};
 
 	programs.home-manager.enable = true;
 	programs.git.enable = true;
 
 	systemd.user.startServices = "sd-switch";
-
-	home.stateVersion = "24.05";
 }

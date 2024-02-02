0db2986bf4e18c317ece80558549dfe08ed1339d
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-02 15:37:57 +0100

    Feature: home-manager importing directly in users, needed because impermanence delete hm in every reboot

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,9 +1,13 @@
-{
+{ pkgs, ...}: {
 	users.users = {
 		user = {
 			initialPassword = "password";
 			isNormalUser = true;
 			extraGroups = ["wheel" "network" "git"];
 		};
+
+		home-manager.users.user = import ../home;
+
+		packages = [ pkgs.home-manager ];
 	};
 }

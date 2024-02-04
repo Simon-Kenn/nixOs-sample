d2729f229849ab6f3c025224d0ff095553933aae
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-04 16:36:31 +0100

    pray again

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -5,8 +5,8 @@
 	sops = {
 		defaultSopsFile = ../secrets/secrets.yaml;
 		age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-		sops.secrets.user-password = {};
-		sops.secrets.wifi-psk = {};
+		secrets.user-password = {};
+		secrets.wifi-psk = {};
 		#age.generateKey = true;
 	};
 }

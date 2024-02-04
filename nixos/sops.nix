f49a01a1427a4a6171923c35eb450bb391b426c0
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-04 18:02:47 +0100

    add opt in persistence on etc key

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -1,11 +1,14 @@
-{inputs, ...}:
-{
+{inputs, config, ...}: let
+	isEd25519 = k: k.type == "ed25519";
+	getKeyPath = k: k.path;
+	keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
+in {
 	imports = [ inputs.sops-nix.nixosModules.sops ];
 
 	sops = {
 		defaultSopsFile = ../secrets/secrets.yaml;
-		age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-		#secrets.user-password = {};
+		age.sshKeyPaths = map getKeyPath keys;
+		secrets.user-password = {};
 		#secrets.wifi-psk = {};
 		#age.generateKey = true;
 	};

afff9d70bc6067e93057888e40398e0157a07fde
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-04 14:39:11 +0100

    adding sshKeyPath age to sops

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -1,4 +1,9 @@
 {inputs, ...}:
 {
 	imports = [ inputs.sops-nix.nixosModules.sops ];
+
+	sops = {
+		defaultSopsFile = ../secrets/secrets.yaml;
+		age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+	};
 }

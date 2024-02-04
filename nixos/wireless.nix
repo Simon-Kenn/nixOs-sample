8b99941177f756a6b05856a34f78c90c069ab26d
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-04 21:54:05 +0100

    pray

--- nixos/wireless.nix
+++ nixos/wireless.nix
@@ -1,17 +1,17 @@
 {config, ...}: {
-	#sops.secrets.wireless.neededForUsers = true;
+	sops.secrets."wireless.env".neededForUsers = true;
 	#sops.secrets."wireless.env" = {};
 	networking = {
 		wireless = {
 			enable = true;
-			#environmentFile = config.sops.secrets."wireless.env".path;
+			environmentFile = config.sops.secrets."wireless.env".path;
 			networks = {
-				"Bbox-5C32B2F3" = {
-					psk = "A2C44EC1ECF3EEAA2471617E2EDC1F";
-				};
-				#"@HOME@" = {
-				#	psk = "@home@";
+				#"Bbox-5C32B2F3" = {
+				#	psk = "A2C44EC1ECF3EEAA2471617E2EDC1F";
 				#};
+				"@HOME@" = {
+					psk = "@home@";
+				};
 			};
 
 			allowAuxiliaryImperativeNetworks = true;

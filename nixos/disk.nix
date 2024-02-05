35173d5f5c4da5f43cb59e09d787247fb7e500a0
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-05 16:44:16 +0100

    prevent security hole message

--- nixos/disk.nix
+++ nixos/disk.nix
@@ -16,6 +16,7 @@
                 type = "filesystem";
 								extraArgs = ["-n NIXBOOT"];
                 format = "vfat";
+								mountOptions = [ "umask=0077" ];
                 mountpoint = "/boot";
               };
             };

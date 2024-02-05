c030893e99a923b5bce87119178503accbd93ca0
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-05 16:37:39 +0100

    separe erasing scripts from disk partition

--- nixos/default.nix
+++ nixos/default.nix
@@ -2,6 +2,7 @@
 {
 	imports = [
 		./hardware-configuration.nix
+		./disk.nix
 		./encrypted-ephemeral-btrfs.nix
 		./impermanence.nix
 		./users.nix
--- nixos/encrypted-ephemeral-btrfs.nix
+++ nixos/encrypted-ephemeral-btrfs.nix
@@ -23,9 +23,6 @@ let
   phase1Systemd = config.boot.initrd.systemd.enable;
 in
 {
-	imports = [
-		inputs.disko.nixosModules.disko
-	];
   boot.initrd = {
     supportedFilesystems = [ "btrfs" ];
     postDeviceCommands = lib.mkIf (!phase1Systemd) (lib.mkBefore wipeScript);
@@ -45,71 +42,4 @@ in
       script = wipeScript;
     };
   };
-
-	disko.devices = {
-    disk = {
-			sda = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-Micron_1100_MTFDDAV256TBN_174619CDC946";
-        content = {
-          type = "gpt";
-          partitions = {
-            esp = {
-              priority = 1;
-              start = "1M";
-              end = "128M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-								extraArgs = ["-n NIXBOOT"];
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-								type = "luks";
-								name = "crypted";
-								settings = {
-									allowDiscards = true;
-								};
-								content = {
-									type = "btrfs";
-                	extraArgs = [ "-f" "-L NIXROOT"]; 
-									postCreateHook = /* sh */ ''
-										MNTPOINT=$(mktemp -d)
-										mount "/dev/mapper/crypted" "$MNTPOINT" -o subvol=/
-										trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
-										btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
-									'';
-                	subvolumes = {
-										"/root" = {
-                  	  mountpoint = "/";
-											mountOptions = ["compress=zstd" "noatime"];
-                  	};
-                  	"/nix" = {
-                  	  mountpoint = "/nix";
-                  	  mountOptions = [ "compress=zstd" "noatime"];
-                  	};
-                  	"/persist" = {
-                  	  mountpoint = "/persist";
-                  	  mountOptions = [ "compress=zstd" "noatime" ];
-                  	};
-                  	"/swap" = {
-                  	  mountpoint = "/.swapvol";
-                  	  swap.swapfile.size = "20M";
-											mountOptions = [ "noatime" ];
-                  	};
-									};
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-
-	fileSystems."/persist".neededForBoot = true;
 }
